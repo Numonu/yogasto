2e8221e85ca9554c12ec6498ab60a56cf991952b
Numonu <juan.villegas.dev@gmail.com>
2023-09-16 16:39:08 -0500

    feat:create new component for groups 'CostGroup'

---
+++ src/components/molecules/CostGroup.tsx
@@ -0,0 +1,21 @@
+import { BiSolidRightArrow } from "react-icons/bi";
+
+type CostGroupTypes = {
+    title : string,
+}
+export default function CostGroup({title}:CostGroupTypes) {
+	return (
+		<li>
+			<button className="bg-zinc-700 outline-zinc-700 w-full p-4 rounded-md outline-2 group hover:bg-zinc-800 hover:outline">
+				<header className="flex items-center justify-between">
+					<h3 className="text-neutral-400 font-semibold group-hover:text-neutral-200">
+						{title}
+					</h3>
+					<button className="text-white group-hover:translate-x-2 transition-transform">
+						<BiSolidRightArrow />
+					</button>
+				</header>
+			</button>
+		</li>
+	);
+}
