07f11abe0f378a266e386cd3173159e924efffd7
Numonu <juan.villegas.dev@gmail.com>
2023-09-16 15:06:30 -0500

    fix:fix route to depependencie in 'main'

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './global/styles/defaults.css'
+import './styles/defaults.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
