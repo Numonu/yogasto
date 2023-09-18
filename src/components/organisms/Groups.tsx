9612e1caff6a2da02af7fe36e3ddf081491eab21
Numonu <juan.villegas.dev@gmail.com>
2023-09-18 17:10:17 -0500

    update:update name to some referencies in 'Groups'

--- src/components/organisms/Groups.tsx
+++ src/components/organisms/Groups.tsx
@@ -1,4 +1,4 @@
-import CostGroup from "../molecules/CostGroup";
+import GroupItem from "../molecules/GroupItem";
 import { FaLayerGroup } from "react-icons/fa";
 
 export default function CostsAtlas() {
@@ -8,12 +8,12 @@ export default function CostsAtlas() {
 				Grupos
 			</h2>
 			<ul className="flex flex-col gap-3 mb-4">
-				<CostGroup title="Gastos Necesarios" />
-				<CostGroup title="Juegos y otras cosas" />
-				<CostGroup title="Comida" />
-				<CostGroup title="Skins y Pases de batalla" />
-				<CostGroup title="Ropa de mi y mi familia" />
-				<CostGroup title="Cosmeticos para la sala" />
+				<GroupItem title="Gastos Necesarios" />
+				<GroupItem title="Juegos y otras cosas" />
+				<GroupItem title="Comida" />
+				<GroupItem title="Skins y Pases de batalla" />
+				<GroupItem title="Ropa de mi y mi familia" />
+				<GroupItem title="Cosmeticos para la sala" />
 			</ul>
 			<button className="bg-indigo-500 text-white flex items-center gap-2 p-2 rounded-md hover:bg-indigo-600">
 				<span className="text-sm">
