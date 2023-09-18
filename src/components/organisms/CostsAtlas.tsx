59fd6323df1b3bdce875d9455f4422a2dbb7b3cc
Numonu <juan.villegas.dev@gmail.com>
2023-09-18 16:50:56 -0500

    feat:add some example components and change style for button in to 'CostAtlas'

--- src/components/organisms/CostsAtlas.tsx
+++ src/components/organisms/CostsAtlas.tsx
@@ -1,21 +1,25 @@
 import CostGroup from "../molecules/CostGroup";
+import { FaLayerGroup } from "react-icons/fa";
 
 export default function CostsAtlas() {
 	return (
-		<aside>
+		<aside className="row-start-1 row-end-3">
 			<h2 className="text-neutral-300 mb-4 text-lg font-semibold">
 				Grupos
 			</h2>
 			<ul className="flex flex-col gap-3 mb-4">
-				<CostGroup title="Gastos Necesarios"/>
-				<CostGroup title="Juegos y otras cosas"/>
-				<CostGroup title="Comida"/>
-				<CostGroup title="Skins y Pases de batalla"/>
-				<CostGroup title="Ropa de mi y mi familia"/>
-				<CostGroup title="Cosmeticos para la sala"/>
+				<CostGroup title="Gastos Necesarios" />
+				<CostGroup title="Juegos y otras cosas" />
+				<CostGroup title="Comida" />
+				<CostGroup title="Skins y Pases de batalla" />
+				<CostGroup title="Ropa de mi y mi familia" />
+				<CostGroup title="Cosmeticos para la sala" />
 			</ul>
-			<button className="outline-indigo-500 text-indigo-500 p-2 outline-2 outline text-sm rounded-md font-semibold hover:bg-indigo-500 hover:text-white hover:outline-white active:scale-90 transition-[colors_transform]">
-				Añadir Nuevo Grupo
+			<button className="bg-indigo-500 text-white flex items-center gap-2 p-2 rounded-md">
+				<span className="text-sm">
+					<FaLayerGroup />
+				</span>
+				<span className="text-sm">Crear Grupo</span>
 			</button>
 		</aside>
 	);
