c9ee55a1500d08c05cd0c03514f15b47058ec00f
Numonu <juan.villegas.dev@gmail.com>
2023-09-18 18:26:12 -0500

    fix:fix style error in hover from 'Dialog'

--- src/components/atoms/Dialog.tsx
+++ src/components/atoms/Dialog.tsx
@@ -8,7 +8,7 @@ export default function Dialog({ children, owner }: DialogTypes) {
 	return (
 		<div className="group relative">
 			{owner}
-			<article className="bg-neutral-800 p-4 absolute left-0 bottom-0 -translate-x-full translate-y-full rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
+			<article className="bg-neutral-800 p-4 absolute left-0 bottom-0 -translate-x-full translate-y-full rounded-md pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity">
 				{children}
 			</article>
 		</div>
