6b65e8e1c0e2777a6914edfddcd8d91dc6801252
Numonu <juan.villegas.dev@gmail.com>
2023-09-20 13:40:17 -0500

    update:update data structure for 'CoreContext'

--- src/context/CoreContex.tsx
+++ src/context/CoreContex.tsx
@@ -2,15 +2,17 @@ import { createContext } from "react";
 import { DEFCORE } from "./constants/DefaultCore";
 
 type TCoreContextItem = {
-    order : number
-	title: string;
-    price : string;
-};
-type TCoreContext = {
 	order: number;
 	title: string;
-	items: TCoreContextItem[];
+	price: string;
+};
+type TCoreContext = {
+	focus: number;
+	storage: {
+		order: number;
+		title: string;
+		items: TCoreContextItem[];
+	}[];
 };
 
-export const CoreContext = createContext<TCoreContext[]>(DEFCORE);
-
+export const CoreContext = createContext<TCoreContext>(DEFCORE);
