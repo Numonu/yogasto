c5ac644580676dbcf0a02981afb7c39141569949
Numonu <juan.villegas.dev@gmail.com>
2023-09-20 14:50:13 -0500

    feat:update data structure and add handler methods to 'CoreProvider'

--- src/context/providers/CoreProvider.tsx
+++ src/context/providers/CoreProvider.tsx
@@ -1,9 +1,46 @@
+import { useCallback, useState } from "react";
 import { CoreContext } from "../CoreContex";
 import { ReactNode } from "react";
 import { DEFCORE } from "../constants/DefaultCore";
+import { produce } from "immer";
 
 export default function CoreProvider({ children }: { children: ReactNode }) {
-	return <CoreContext.Provider value={DEFCORE}>
-        {children}
-    </CoreContext.Provider>;
+	const [core, setCore] = useState(DEFCORE.core);
+
+	const handleAddGroup = useCallback((title : string) => {
+		setCore(
+			produce((draft) => {
+				draft.storage.push({
+					order: draft.storage.length,
+					title,
+					items: [],
+				});
+			})
+		);
+	}, []);
+
+    const handleAddItem = useCallback((title : string , price : string) => {
+		setCore(
+			produce((draft) => {
+                const itemAtlas = draft.storage[draft.focus].items;
+				itemAtlas.push({
+                    order : itemAtlas.length,
+                    title,
+                    price,
+                })
+			})
+		);
+	}, []);
+
+	return (
+		<CoreContext.Provider
+			value={{
+				core,
+				handleAddGroup,
+				handleAddItem,
+			}}
+		>
+			{children}
+		</CoreContext.Provider>
+	);
 }
