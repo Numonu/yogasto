f66fe297e9fcd5fe5bcdaa0e6d5e1f8563412c2a
Numonu <juan.villegas.dev@gmail.com>
2023-09-20 16:15:46 -0500

    feat:add new prop to set title in 'Modal'

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -5,9 +5,10 @@ import { MdOutlineCancel } from "react-icons/md";
 type ModalTypes = {
 	children: ReactNode;
 	onClose: () => void;
+    title? : string;
 };
 
-const Modal: FC<ModalTypes> = ({ children, onClose }) => {
+const Modal: FC<ModalTypes> = ({ children, onClose , title }) => {
 	return createPortal(
 		<div
 			className="bg-black bg-opacity-50 backdrop-blur-sm fixed left-0 top-0 w-screen h-screen px-4 grid place-items-center"
@@ -17,7 +18,8 @@ const Modal: FC<ModalTypes> = ({ children, onClose }) => {
 				className="bg-zinc-800 w-full max-w-[600px] p-4 rounded-md"
 				onClick={(e) => e.stopPropagation()}
 			>
-                <div className="flex justify-end">
+                <div className="flex justify-between items-center mb-4">
+                    <h2 className="text-neutral-300 text-lg">{title}</h2>
                     <button className="text-neutral-300 text-3xl" onClick={onClose}>
                         <MdOutlineCancel/>
                     </button>
