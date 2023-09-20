1344c447e5ef2a4bbbfeb6c101a664ae620efecf
Numonu <juan.villegas.dev@gmail.com>
2023-09-20 13:20:04 -0500

    feat:import and use store for total in 'Total'

--- src/components/molecules/Total.tsx
+++ src/components/molecules/Total.tsx
@@ -1,6 +1,10 @@
 import { BsCurrencyDollar } from "react-icons/bs";
+import useTotal from "../../hooks/useTotal";
 
 export default function Total() {
+
+	const {total , updateTotal} = useTotal((state) => state);
+
 	return (
 		<div className="text-neutral-400 outline-indigo-500 max-w-sm px-2 py-4 flex gap-2 items-center text-3xl outline outline-1 rounded-md">
 			<span className="">
@@ -10,7 +14,9 @@ export default function Total() {
 				className="bg-transparent outline-none min-w-0"
 				type="number"
 				step={0.01}
-                placeholder="0.00"
+				value={total}
+                defaultValue={0}
+				onChange={e => updateTotal(Number(e.target.value))}
 			/>
 		</div>
 	);
