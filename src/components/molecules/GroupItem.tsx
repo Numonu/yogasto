ed3f5f5a4db810eb08e86c6f9389671674a67a0b
Numonu <juan.villegas.dev@gmail.com>
2023-09-22 18:11:43 -0500

    feat:add custom styles if group is selected in 'GroupItem'

--- src/components/molecules/GroupItem.tsx
+++ src/components/molecules/GroupItem.tsx
@@ -7,15 +7,28 @@ type CostGroupTypes = {
 	order: number;
 };
 export default function CostGroup({ title, order }: CostGroupTypes) {
-	const { handleRemoveGroup, handleChangeFocus } = useContext(CoreContext);
+	const {
+		handleRemoveGroup,
+		handleChangeFocus,
+		core: { focus },
+	} = useContext(CoreContext);
+
+	const focusMe = focus == order;
+
 	return (
 		<li>
 			<button
-				className="bg-zinc-700 outline-zinc-700 w-full p-4 pr-2 rounded-md outline-2 group hover:bg-zinc-800 hover:outline"
+				className={`bg-zinc-700 outline-zinc-700 w-full p-4 pr-2 rounded-md outline-2 group hover:bg-zinc-800 hover:outline ${
+					focusMe && "bg-zinc-800 outline"
+				}`}
 				onClick={() => handleChangeFocus(order)}
 			>
 				<header className="flex items-center justify-between">
-					<h3 className="text-neutral-400 group-hover:text-neutral-200">
+					<h3
+						className={`text-neutral-400 group-hover:text-neutral-200 ${
+							focusMe && "text-neutral-200"
+						}`}
+					>
 						{title}
 					</h3>
 					<button
