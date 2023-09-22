32a27250c72d0a099a9c0bbb76a219bc73cca757
Numonu <juan.villegas.dev@gmail.com>
2023-09-22 18:30:26 -0500

    feat:redere all group item expenses or profits in 'Resume'

--- src/components/molecules/Resume.tsx
+++ src/components/molecules/Resume.tsx
@@ -1,4 +1,11 @@
+import { CoreContext } from "../../context/CoreContex";
+import useTotal from "../../hooks/useTotal";
+import { useContext } from "react";
+
 export default function Resume() {
+	const total = useTotal((state) => state.total);
+	const { core } = useContext(CoreContext);
+
 	return (
 		<div className="text-white">
 			<div className="border-neutral-300 border-b pb-1 mb-1 flex items-center gap-4">
@@ -6,13 +13,18 @@ export default function Resume() {
 					<span className="text-neutral-300">Operaciones : </span>
 				</div>
 				<div className="grow flex items-center justify-end gap-4">
-					<span className="text-red-500 text-xs">- $ 32.35</span>
-					<span className="text-emerald-500 text-xs">+ $ 10.35</span>
-					<span className="text-lg font-light">$ 75.50</span>
+					{core.storage.map((e) => {
+						return e.items.map((e) => {
+							return <span className={`text-xs ${e.type == "expense" ? "text-red-500" : "text-emerald-500"}`}>
+								{e.type == "expense" ? "-" : "+"} $ {e.price}
+							</span>;
+						});
+					})}
+					<span className="text-lg font-light">$ {total}</span>
 				</div>
 			</div>
 			<div className="flex justify-end">
-				<span className="text-neutral-400 text-base">$ 125.34</span>
+				<span className="text-neutral-400 text-base">$ 124.00</span>
 			</div>
 		</div>
 	);
