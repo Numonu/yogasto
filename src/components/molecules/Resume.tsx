2150921629b6ed5aeda1322ca23d1926821d72d0
Numonu <juan.villegas.dev@gmail.com>
2023-09-22 18:40:08 -0500

    feat:add resume function to set result of all operations in 'Resume'

--- src/components/molecules/Resume.tsx
+++ src/components/molecules/Resume.tsx
@@ -6,6 +6,18 @@ export default function Resume() {
 	const total = useTotal((state) => state.total);
 	const { core } = useContext(CoreContext);
 
+	const printResume = () => {
+		let result = total;
+		core.storage.forEach((e) => {
+			return e.items.forEach((e) => {
+				const price = Number(e.price);
+				if (e.type == "expense") result -= price;
+				else result += price;
+			});
+		});
+		return String(result).slice(0,5);
+	};
+
 	return (
 		<div className="text-white">
 			<div className="border-neutral-300 border-b pb-1 mb-1 flex items-center gap-4">
@@ -15,16 +27,27 @@ export default function Resume() {
 				<div className="grow flex items-center justify-end gap-4">
 					{core.storage.map((e) => {
 						return e.items.map((e) => {
-							return <span className={`text-xs ${e.type == "expense" ? "text-red-500" : "text-emerald-500"}`}>
-								{e.type == "expense" ? "-" : "+"} $ {e.price}
-							</span>;
+							return (
+								<span
+									className={`text-xs ${
+										e.type == "expense"
+											? "text-red-500"
+											: "text-emerald-500"
+									}`}
+								>
+									{e.type == "expense" ? "-" : "+"} ${" "}
+									{e.price}
+								</span>
+							);
 						});
 					})}
 					<span className="text-lg font-light">$ {total}</span>
 				</div>
 			</div>
 			<div className="flex justify-end">
-				<span className="text-neutral-400 text-base">$ 124.00</span>
+				<span className="text-neutral-400 text-base">
+					$ {printResume()}
+				</span>
 			</div>
 		</div>
 	);
