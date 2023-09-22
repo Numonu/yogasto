020382a26a0e9882d3a057e8bc56723d76125f29
Numonu <juan.villegas.dev@gmail.com>
2023-09-22 11:54:40 -0500

    feat:add logic to display modal to create new items in 'Costs'

--- src/components/organisms/Costs.tsx
+++ src/components/organisms/Costs.tsx
@@ -1,40 +1,62 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Item from "../molecules/CostItem";
 import { MdOutlineDoubleArrow } from "react-icons/md";
 import Legend from "../molecules/Legend";
 import { CoreContext } from "../../context/CoreContex";
+import useBoolean from "../../hooks/useBoolean";
+import CreateItem, { TItem } from "../molecules/CreateItem";
 
 export default function ItemAtlas() {
 	const { core } = useContext(CoreContext);
+	const { control, controlOn, controlOff } = useBoolean();
+
+	const [type, setType] = useState<TItem>("profit");
+
+	const openWithType = (newType : TItem) => {
+		controlOn();
+		setType(newType);
+	}
+
 	return (
-		<div>
-			<header className="flex justify-between items-center">
-				<h2 className="text-neutral-300 mb-4 text-lg font-semibold">
-					Gastos
-				</h2>
-				<Legend>
-					Declarar lo que genera ganancias y pérdidas nos ayuda a
-					entender qué impulsar y qué mejorar en nuestro camino hacia
-					el éxito financiero
-				</Legend>
-			</header>
-			<ul className="flex flex-col gap-3 mb-4">
-				{ core.storage[core.focus].items.map(e => <Item title={e.title} cost={e.price}/>) }
-			</ul>
-			<div className="flex gap-4">
-				<button className="bg-emerald-500 text-white flex gap-2 p-2 rounded-md hover:bg-emerald-600">
-					<span className="text-lg -rotate-90">
-						<MdOutlineDoubleArrow />
-					</span>
-					<span className="text-sm">Añadir Ganancia</span>
-				</button>
-				<button className="bg-red-500 text-white flex gap-2 p-2 rounded-md hover:bg-red-600">
-					<span className="text-lg rotate-90">
-						<MdOutlineDoubleArrow />
-					</span>
-					<span className="text-sm">Añadir Gasto</span>
-				</button>
+		<>
+			<div>
+				<header className="flex justify-between items-center">
+					<h2 className="text-neutral-300 mb-4 text-lg font-semibold">
+						Gastos
+					</h2>
+					<Legend>
+						Declarar lo que genera ganancias y pérdidas nos ayuda a
+						entender qué impulsar y qué mejorar en nuestro camino
+						hacia el éxito financiero
+					</Legend>
+				</header>
+				<ul className="flex flex-col gap-3 mb-4">
+					{core.storage[core.focus].items.map((e) => (
+						<Item title={e.title} cost={e.price} />
+					))}
+				</ul>
+				<div className="flex gap-4">
+					<button
+						className="bg-emerald-500 text-white flex gap-2 p-2 rounded-md hover:bg-emerald-600"
+						onClick={() => openWithType("profit")}
+					>
+						<span className="text-lg -rotate-90">
+							<MdOutlineDoubleArrow />
+						</span>
+						<span className="text-sm">Añadir Ganancia</span>
+					</button>
+					<button
+						className="bg-red-500 text-white flex gap-2 p-2 rounded-md hover:bg-red-600"
+						onClick={() => openWithType("expense")}
+					>
+						<span className="text-lg rotate-90">
+							<MdOutlineDoubleArrow />
+						</span>
+						<span className="text-sm">Añadir Gasto</span>
+					</button>
+				</div>
 			</div>
-		</div>
+			{control && <CreateItem onClose={controlOff} type={type} />}
+		</>
 	);
 }
