f8facf18e16353ef7c61401f156fa2823ab4613f
Numonu <juan.villegas.dev@gmail.com>
2023-09-22 17:51:19 -0500

    feat:add delete button and conection with provider in 'GroupItem'

--- src/components/molecules/GroupItem.tsx
+++ src/components/molecules/GroupItem.tsx
@@ -1,18 +1,25 @@
-import { BsFolder2Open } from "react-icons/bs";
+import { useContext } from "react";
+import { BsTrash3Fill } from "react-icons/bs";
+import { CoreContext } from "../../context/CoreContex";
 
 type CostGroupTypes = {
-    title : string,
-}
-export default function CostGroup({title}:CostGroupTypes) {
+	title: string;
+	order: number;
+};
+export default function CostGroup({ title, order }: CostGroupTypes) {
+	const { handleRemoveGroup } = useContext(CoreContext);
 	return (
 		<li>
-			<button className="bg-zinc-700 outline-zinc-700 w-full p-4 rounded-md outline-2 group hover:bg-zinc-800 hover:outline">
+			<button className="bg-zinc-700 outline-zinc-700 w-full p-4 pr-2 rounded-md outline-2 group hover:bg-zinc-800 hover:outline">
 				<header className="flex items-center justify-between">
 					<h3 className="text-neutral-400 group-hover:text-neutral-200">
 						{title}
 					</h3>
-					<button className="text-white group-hover:translate-x-2 transition-transform">
-						<BsFolder2Open />
+					<button
+						className="text-neutral-400 p-2 hover:text-red-500"
+						onClick={() => handleRemoveGroup(order)}
+					>
+						<BsTrash3Fill />
 					</button>
 				</header>
 			</button>
